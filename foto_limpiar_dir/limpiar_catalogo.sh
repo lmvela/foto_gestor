b87440cd9cbb8ccf747ae82080ca0a29f6bc8bf8
lmvela <lmvela@yahoo.com>
2022-06-11 23:33:28 +0200

    Añadido echo/pausa al sh de limpiar dir catalogo

--- foto_limpiar_dir/limpiar_catalogo.sh
+++ foto_limpiar_dir/limpiar_catalogo.sh
@@ -1,8 +1,14 @@
 # Delete hidden files (starting with .)
-find /media/cavehost_hdd/00_fotos/catalogo -type f -iname '.*' -exec rm -f {} \+
+echo Moviendo ficheros ocultos a la papelera
+find /media/cavehost_hdd/00_fotos/catalogo -type f -iname '.*' -exec mv -f {} /media/cavehost_hdd/00_fotos/descartar/papelera/ \;
+read -p "Press any key to resume ..."
 
 # Move files with no extension to papelera
+echo Moviendo ficheros sin extension a la papelera
 find /media/cavehost_hdd/00_fotos/catalogo -type f ! -name '*.*' -exec mv -i {} /media/cavehost_hdd/00_fotos/descartar/papelera/ \;
+read -p "Press any key to resume ..."
 
 # Move temp jpg~ to papelera
+echo Moviendo jpg~ a la papelera
 find /media/cavehost_hdd/00_fotos/catalogo -type f -name '*.jpg~' -exec mv -i {} /media/cavehost_hdd/00_fotos/descartar/papelera/ \;
+read -p "Press any key to resume ..."
