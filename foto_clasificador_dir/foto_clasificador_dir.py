7a969b0a15568a6fb2996ecc0705d85c469268f1
Luis Miguel Vela <l.vela@kostal.com>
2022-07-06 18:13:11 +0200

    Improved filename datatime identification

--- foto_clasificador_dir/foto_clasificador_dir.py
+++ foto_clasificador_dir/foto_clasificador_dir.py
@@ -1,3 +1,5 @@
+from PIL import Image, ExifTags
+from datetime import datetime
 import os
 import sys
 import re
@@ -10,65 +12,103 @@ from foto_comun.foto_comun import *
 from foto_db.foto_db import *
 
 ##
+# CONSTANTS
+##
+
+## REGEX patters for filenames with datetime information in 7 groups: (discard)(4:year)(2:mes)(2:dia) (2:h)(2:m)(2:s)
+pattern_fn_dtinfo_list = [   
+    r'(\\C360)_([0-9]{4})-([0-9]{2})-([0-9]{2})-([0-9]{2})-([0-9]{2})-([0-9]{2})-[0-9]{3}\.', # CAMARA   
+    r'(\\)([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2})\.([0-9]{2})\.([0-9]{2})\.',  # IPAD 
+    r'(IMG|VID)_([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})([0-9]{2})',
+    r'(\\PANO)_([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})([0-9]{2})', # PANASONIC camara
+    r'(\\)([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})([0-9]{2})\.', # Camara Samsung S20FE    
+    r'(VID|IMG)-([0-9]{4})([0-9]{2})([0-9]{2})-WA([0-9]{1})([0-9]{1})([0-9]{1})', # Whatsapp
+]
+
+## REGEX patterns for filenames without datetime information
+pattern_fn_nodtinfo_list = [
+    r'MVI_[0-9]{4}\.',
+    r'P1[0-9]{6}\.',
+    r'DSCN[0-9]{3}\.',
+    r'DSC_[0-9]{4}\.',
+    r'DSC[0-9]{5}\.',
+    r'Imagen [0-9]{3}\.',
+    r'IMG_[0-9]{4}\.',
+    r'IMG [0-9]{4}\.',
+    r'IMG[0-9]{4}\.',
+    r'MOV[0-9]{3}\.'
+]
+
+##
+#
+##
+def create_fn_dt(res_groups):
+    dttime_ints = [int(x) for x in res_groups]
+    return (TAG_DT_ORIGIN_FN, datetime(dttime_ints[0], dttime_ints[1], dttime_ints[2], dttime_ints[3], dttime_ints[4], dttime_ints[5]))
+
+##
+#
+##
+def os_creation_date(path_to_file):
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if sys.platform.startswith('win'):
+        return datetime.fromtimestamp(os.path.getctime(path_to_file))
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return datetime.fromtimestamp(stat.st_birthtime)
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return datetime.fromtimestamp(stat.st_mtime)
+
+##
+#
+##
+def read_fn_dt(fn):
+    try:
+        # First try EXIF info
+        img = Image.open(fn)
+        img_exif = img.getexif()
+        if img_exif is not None:
+            for key, val in img_exif.items():
+                if key in ExifTags.TAGS:
+                    print(f'{ExifTags.TAGS[key]}:{val}')            
+                if key == 0x0132 or key == 0x9003:   #DateTime or DateTimeOriginal
+                    return (TAG_DT_ORIGIN_EXIF, datetime.strptime(val, '%Y:%m:%d %H:%M:%S'))
+    except:
+        pass    # Carry on checking other metadata extraction methode
+
+    # If no EXIF: take the file date / time
+    print("No EXIF: " + fn)
+    return (TAG_DT_ORIGIN_OS, os_creation_date(fn))
+
+##
 #
 ##
 def get_fn_source(fn):
-    # CAMARA
-    x=re.compile('C360_[0-9]{4})-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{3}\.')
-    if x.match(fn):      
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}\.[0-9]{2}\.[0-9]{2}\.')               # IPAD
-    if x.match(fn):      
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('{IMG|VID}_[0-9]{8}_[0-9]{6}')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('PANO_[0-9]{8}_[0-9]{6}')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('MVI_[0-9]{4}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('P1[0-9]{6}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('[0-9]{8}_[0-9]{6}\.')   # Camara Smsung S20FE
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA        
-    x=re.compile('DSCN[0-9]{3}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('DSC_[0-9]{4}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('DSC[0-9]{5}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('Imagen [0-9]{3}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('IMG_[0-9]{4}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('IMG [0-9]{4}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('IMG[0-9]{4}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
-    x=re.compile('MOV[0-9]{3}\.')
-    if x.match(fn):
-        return TAG_ORIGEN_CAMARA
 
-    #Whatsapp
-    x=re.compile('IMG-[0-9]{8}-WA[0-9]{4}')
-    if x.match(fn):
-        return TAG_ORIGEN_WAPP
-    x=re.compile('VID-[0-9]{8}-WA[0-9]{4}')
-    if x.match(fn):
-        return TAG_ORIGEN_WAPP
+    # CHECK if Filename contains date time info:
+    for pattern in pattern_fn_dtinfo_list:
+        res=re.search(pattern, fn)
+        if res:
+            res_groups = res.groups(1)[1:]  # Remove firs element in tuple. contains discard info
+            return create_fn_dt(res_groups), TAG_ORIGEN_CAMARA
+        
+    # Media files without datetime info in the file name.
+    # Try to extract the datetime from the file itself (EXIF, OS datetime, other?)
+    media_dt = read_fn_dt(fn)
+    for pattern in pattern_fn_nodtinfo_list:
+        res=re.search(pattern, fn)
+        if res:
+            return media_dt, TAG_ORIGEN_CAMARA     
 
-    return TAG_ORIGEN_DESCONOCIDO
+    # No info available leave default results
+    return media_dt, TAG_ORIGEN_DESCONOCIDO
 
 ##
 #
@@ -91,8 +131,8 @@ def clasificacion_completa_desde_zero(dir_a_clasificar):
 
                 if exists_img is None:
                     # Es un fichero nuevo que no existe en BD
-                    media_origen = get_fn_source(fn_hash_sz[0])
-                    add_img_catalogo_db(fn_hash_sz, user, tag_tipo, media_origen)
+                    media_dt, media_origen = get_fn_source(fn_hash_sz[0])
+                    add_img_catalogo_db(fn_hash_sz, user, tag_tipo, media_origen, media_dt)
                 elif exists_img['filename'] == fn_hash_sz[0]:
                     # Es un fichero que ya esta en la BD
                     img_existing_ok_db(exists_img)
