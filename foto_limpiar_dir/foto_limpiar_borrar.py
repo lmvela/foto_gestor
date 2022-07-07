2491d479a21a127aa7c5fcb49f004ec6829f99b1
lmvela <lmvela@yahoo.com>
2022-07-07 23:05:22 +0200

    Clean functions reorganized

---
+++ foto_limpiar_dir/foto_limpiar_borrar.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+if sys.platform.startswith('win'):
+    sys.path.append("C:\\work\\02_Pers\\proyectos\\foto_gestor")
+else:
+    sys.path.append("/home/luis/Documentos/02_projects/fotogest/foto_gestor")
+from foto_comun.foto_comun import *
+from foto_db.foto_db import *
+
+##
+#
+##
+def borrar_fichero(borr):
+    # mover fichero a papelera
+    origen = borr['filename']
+    destino = os.path.join(PAPELERA_DIR, nombre_fichero(origen))
+    try:
+        shutil.move(origen, destino)
+        print("MV OK: " + origen + " -> " + destino)
+        # Eliminar documento DB
+        ret = del_borrar_db(borr)
+        print("DB borrar removed: " + str(ret))    
+    except:
+        print("MV ERROR: " + origen + " -> " + destino)
+
+##
+# Accedemos a la colecion lista_duplicados: movemos el fichero filename a papelera y eliminamos entrada en BD
+##
+def main():
+    list_borrar = get_all_borrar_db()
+    for borr in list_borrar:
+        borrar_fichero(borr)
+
+if __name__ == "__main__":
+    main()
---
+++ foto_limpiar_dir/previa_limpiar_catalogo.sh
@@ -0,0 +1,16 @@
+# For files in Catalogue
+
+# Delete hidden files (starting with .)
+echo Moviendo ficheros ocultos a la papelera
+find /media/cavehost_hdd/00_fotos/catalogo -type f -iname '.*' -exec mv -f {} /media/cavehost_hdd/00_fotos/papelera/ \;
+read -p "Press any key to resume ..."
+
+# Move files with no extension to papelera
+echo Moviendo ficheros sin extension a la papelera
+find /media/cavehost_hdd/00_fotos/catalogo -type f ! -name '*.*' -exec mv -i {} /media/cavehost_hdd/00_fotos/papelera/ \;
+read -p "Press any key to resume ..."
+
+# Move temp jpg~ to papelera
+echo Moviendo jpg~ a la papelera
+find /media/cavehost_hdd/00_fotos/catalogo -type f -name '*.jpg~' -exec mv -i {} /media/cavehost_hdd/00_fotos/papelera/ \;
+read -p "Press any key to resume ..."
